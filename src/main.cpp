549dd31825a7d3af62651347b7c4972af9ba10a7
hemanta-gayen <hematagayan2000@gmail.com>
2026-09-13 23:33:31 +0530

    Testing helper_server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
 
-int main() {
+
+int main(int argc, char* argv[]) {
+    for (int i = 1; i < argc; ++i) {
+        if (std::string(argv[i]) == "--helper-server") {
+            // return guchho::helper_server::RunHelperServer();
+            return 0;
+        }
+    }
+
     std::cout << "Hello from Guchho!\n";
     return 0;
 }

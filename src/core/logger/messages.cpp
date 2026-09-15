4ae5c05a1f587410e6417d061124185c19ea4ffe
hemanta-gayen <hematagayan2000@gmail.com>
2026-09-15 19:42:05 +0530

    Update messages

--- src/core/logger/messages.cpp
+++ src/core/logger/messages.cpp
@@ -279,7 +279,7 @@ namespace guchho::logger {
         case MsgCat::kJS_ImportExpressionNotRecognized: return "This \"import()\" was not recognized because {}";
         case MsgCat::kJS_ImportNotBundledNotStringLiteral: return "This \"import\" expression will not be bundled because the argument is not a string literal";
         case MsgCat::kJS_DirectEval: return "Using direct eval with a bundler is not recommended and may cause problems";
-        case MsgCat::kJS_DirectEvalNote: return "You can read more about direct eval and bundling here: https://esbuild.github.io/link/direct-eval";
+        case MsgCat::kJS_DirectEvalNote: return "See the documentation for more information about direct eval and bundling";
         case MsgCat::kJS_ConvertingRequireToESM: return "Converting require() calls to ESM is not supported";
         case MsgCat::kJS_RequireNotBundledNotStringLiteral: return "This \"require\" expression will not be bundled because the argument is not a string literal";
         case MsgCat::kJS_RequireNotBundledArgCount: return "Calling require() with the wrong number of arguments is not supported";
